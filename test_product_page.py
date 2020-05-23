698d26656b10ea4d58a0f3f8f8dac676ab09e4f7
wonder-SMA <wonder2012mail@gmail.com>
2020-05-23 23:35:54 +0400

    xfail test

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,10 @@
 from .pages.product_page import ProductPage
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+
+@pytest.mark.parametrize("number", ["1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+def test_guest_can_add_product_to_basket(browser, number):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{number}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
